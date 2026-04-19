e6f3cf4bf6644009ff590e3cba7a31555232d9ac
Victor Hugo <csivictorhugo@gmail.com>
2026-04-18 23:40:52 -0300

    feat: api structure using postman app for test

--- src/controllers/podcasts-controller.ts
+++ src/controllers/podcasts-controller.ts
@@ -2,7 +2,18 @@ import { IncomingMessage, ServerResponse} from 'http';
 
 export const getListEpisodes = async (request: IncomingMessage, response: ServerResponse) => {
   response.writeHead(200, {'content-type': "application/json"})
-  response.end({
-    name: "Victor Hugo",
-  })
+  response.end(JSON.stringify([
+    {
+      podcastName: "Flow",
+      episode: "FABIO AKITA - Flow #588",
+      videoId: "4c7pbOxYn_A",
+      categories: ["dev", "AI"]
+    },
+    {
+      podcastName: "Inteligência Ltda",
+      episode: "SUPERINTELIGÊNCIA ARTIFICIAL - AKITA, ROBERTA E CAVALLINI - Inteligência Ltda.Podcast #1583",
+      videoId: "_Hl9wiLkns4",
+      categories: ["dev", "AI"]
+    }
+  ]))
 }
