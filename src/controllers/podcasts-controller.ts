84675e67d8d9f5404938566a65b3debf9abe15f8
Victor Hugo <csivictorhugo@gmail.com>
2026-04-21 00:05:08 -0300

    refactor: killing magic numbers and floating texts using new enum layers

--- src/controllers/podcasts-controller.ts
+++ src/controllers/podcasts-controller.ts
@@ -1,15 +1,17 @@
 import { IncomingMessage, ServerResponse} from 'http';
 import { serviceListEpisodes } from '../services/list-episodes-service';
 import { serviceFilterEpisodes } from '../services/filter-episodes-service';
+import { StatusCode } from '../utils/status-code';
+import { ContentType } from '../utils/content-type';
 
 export const getListEpisodes = async (request: IncomingMessage, response: ServerResponse) => {
   const content = await serviceListEpisodes();
-  response.writeHead(200, {'content-type': "application/json"})
+  response.writeHead(StatusCode.OK, {'content-type': ContentType.JSON})
   response.end(JSON.stringify(content))
 }
 
 export const getFilterEpisodes = async (request: IncomingMessage, response: ServerResponse) => {
   const content = await serviceFilterEpisodes(request.url);
-  response.writeHead(200, {'content-type': "application/json"})
+  response.writeHead(StatusCode.OK, {'content-type': ContentType.JSON})
   response.end(JSON.stringify(content))
 }
--- src/repositories/podcasts-repository.ts
+++ src/repositories/podcasts-repository.ts
@@ -6,7 +6,8 @@ import PodcastModel from "../models/podcast-model";
 const pathData = path.join(__dirname, "../repositories/podcasts.json");
 
 export const repositoryPodcast = async (podcastName?: string): Promise<Podcast[]> => {
-  const rawData = fs.readFileSync(pathData, "utf-8")
+  const language = "utf-8";
+  const rawData = fs.readFileSync(pathData, language)
   let jsonFile = JSON.parse(rawData)
 
   if(podcastName) {
