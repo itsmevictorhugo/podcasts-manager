35a8f909f46b1113caf2aea64c94348358c67841
Victor Hugo <csivictorhugo@gmail.com>
2026-04-21 01:03:19 -0300

    feat: error handling restructuring duties and improving code quality between services controllers and models

--- src/controllers/podcasts-controller.ts
+++ src/controllers/podcasts-controller.ts
@@ -1,17 +1,18 @@
 import { IncomingMessage, ServerResponse} from 'http';
 import { serviceListEpisodes } from '../services/list-episodes-service';
 import { serviceFilterEpisodes } from '../services/filter-episodes-service';
-import { StatusCode } from '../utils/status-code';
 import { ContentType } from '../utils/content-type';
+import { PodcastTransferModel } from '../models/filter-podcast-model';
 
 export const getListEpisodes = async (request: IncomingMessage, response: ServerResponse) => {
-  const content = await serviceListEpisodes();
-  response.writeHead(StatusCode.OK, {'content-type': ContentType.JSON})
-  response.end(JSON.stringify(content))
+  const content: PodcastTransferModel = await serviceListEpisodes();
+  response.writeHead(content.statusCode, {'content-type': ContentType.JSON})
+  response.end(JSON.stringify(content.body))
 }
 
 export const getFilterEpisodes = async (request: IncomingMessage, response: ServerResponse) => {
-  const content = await serviceFilterEpisodes(request.url);
-  response.writeHead(StatusCode.OK, {'content-type': ContentType.JSON})
-  response.end(JSON.stringify(content))
+  const content: PodcastTransferModel = await serviceFilterEpisodes(request.url);
+  response.writeHead(content.statusCode, {'content-type': ContentType.JSON})
+  response.write(JSON.stringify(content.body))
+  response.end()
 }
--- src/services/list-episodes-service.ts
+++ src/services/list-episodes-service.ts
@@ -1,6 +1,18 @@
+import { PodcastTransferModel } from "../models/filter-podcast-model";
 import { repositoryPodcast } from "../repositories/podcasts-repository";
+import { StatusCode } from "../utils/status-code";
 
-export const serviceListEpisodes = async () => {
+export const serviceListEpisodes = async (): Promise<PodcastTransferModel> => {
+    let responseFormat: PodcastTransferModel = {
+      statusCode: 0,
+      body: [],
+    };
   const data = await repositoryPodcast();
-  return data;
+
+  responseFormat = {
+    statusCode: data.length !== 0 ? StatusCode.OK : StatusCode.NO_CONTENT,
+    body: data
+  } 
+
+  return responseFormat;
 }
