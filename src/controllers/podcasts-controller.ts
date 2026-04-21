5e979e3472953127340d7ca5b174cb37c66b640f
Victor Hugo <csivictorhugo@gmail.com>
2026-04-20 23:10:23 -0300

    feat: querystring filtering episodes and simplifing their names for query

--- src/controllers/podcasts-controller.ts
+++ src/controllers/podcasts-controller.ts
@@ -9,7 +9,8 @@ export const getListEpisodes = async (request: IncomingMessage, response: Server
 }
 
 export const getFilterEpisodes = async (request: IncomingMessage, response: ServerResponse) => {
-  const content = await serviceFilterEpisodes('Flow');
+  const queryString = request.url?.split("?p=")[1] || "";
+  const content = await serviceFilterEpisodes(queryString);
   response.writeHead(200, {'content-type': "application/json"})
   response.end(JSON.stringify(content))
 }
