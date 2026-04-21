2e7745d935d65f55f8e246aee530dfe0edae05e0
Victor Hugo <csivictorhugo@gmail.com>
2026-04-21 15:44:10 -0300

    refactor: tiny optimizing on content type

--- src/controllers/podcasts-controller.ts
+++ src/controllers/podcasts-controller.ts
@@ -4,15 +4,17 @@ import { serviceFilterEpisodes } from '../services/filter-episodes-service';
 import { ContentType } from '../utils/content-type';
 import { PodcastTransferModel } from '../models/podcast-transfer-model';
 
+const defaultContent = {'content-type': ContentType.JSON};
+
 export const getListEpisodes = async (request: IncomingMessage, response: ServerResponse) => {
   const content: PodcastTransferModel = await serviceListEpisodes();
-  response.writeHead(content.statusCode, {'content-type': ContentType.JSON})
+  response.writeHead(content.statusCode, defaultContent)
   response.end(JSON.stringify(content.body))
 }
 
 export const getFilterEpisodes = async (request: IncomingMessage, response: ServerResponse) => {
   const content: PodcastTransferModel = await serviceFilterEpisodes(request.url);
-  response.writeHead(content.statusCode, {'content-type': ContentType.JSON})
+  response.writeHead(content.statusCode, defaultContent)
   response.write(JSON.stringify(content.body))
   response.end()
 }
